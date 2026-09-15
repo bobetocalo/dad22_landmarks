377a96a68f573bab0c7d44a4095732e4ef59aacc
Roberto Valle <roberto.valle@upm.es>
2026-09-15 15:52:02 +0200

    Use SSH forwarding for secure private repository cloning

--- test_dad22_landmarks_example.sh
+++ test_dad22_landmarks_example.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 echo 'Using Docker to start the container and run tests ...'
-sudo docker build --force-rm --build-arg SSH_PRIVATE_KEY="$(cat ~/.ssh/id_rsa)" -t dad22_landmarks_image .
-sudo docker volume create --name dad22_landmarks_volume
-sudo docker run --name dad22_landmarks_container -v dad22_landmarks_volume:/home/username --rm --gpus all -it -d dad22_landmarks_image bash
+sudo docker build --force-rm --ssh default=$HOME/.ssh/id_rsa -t dad22_landmarks_image .
+sudo docker run --name dad22_landmarks_container --rm --gpus all -it -d dad22_landmarks_image bash
 sudo docker exec -w /home/username/dad22_landmarks dad22_landmarks_container python test/dad22_landmarks_test.py --input-data test/example.tif --database dad --gpu 0 --save-image
-sudo docker stop dad22_landmarks_container
 echo 'Transferring data from docker container to your local machine ...'
 mkdir -p output
-sudo chown -R "${USER}":"${USER}" /var/lib/docker/
-rsync --delete -azvv /var/lib/docker/volumes/dad22_landmarks_volume/_data/conda/envs/dad22/lib/python3.8/site-packages/images_framework/output/images/ output
-sudo docker system prune --all --force --volumes
-sudo docker volume rm $(sudo docker volume ls -qf dangling=true)
+sudo docker cp dad22_landmarks_container:/home/username/conda/envs/dad22/lib/python3.8/site-packages/images_framework/output/images/. output/
+sudo chown -R "${USER}":"${USER}" output
+sudo docker rm -f dad22_landmarks_container
+sudo docker image rm dad22_landmarks_image
+sudo docker builder prune -a -f
